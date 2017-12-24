1d68f37aefe08ab12464f6ea3bb8a9eeca66ae1b
mslezak <michal_slezak@tempest.sk>
2017-12-24 13:36:09 +0100

    initial quizz attempt

---
+++ src/app/questions/questions.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'questions',
+  templateUrl: './questions.component.html',
+  styleUrls: ['./questions.component.css']
+})
+export class QuestionsComponent implements OnInit {
+
+  number: number = 1;
+  index: number = 0;
+
+  questions: string[] = [
+    "Akej farby je obloha?",
+    "Aky deň je prvý v týždni?",
+    "Koľko dní má december?"
+  ];
+
+  answers: string[] = [
+    "modrá", "červená", "zelená", "fialová",
+    "utorok", "streda", "pondelok", "sobota",
+    "25", "30", "29", "31"    
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitAnswer(): void {
+    this.number++;
+    this.answers;
+    this.index += 4;
+  }
+
+}
