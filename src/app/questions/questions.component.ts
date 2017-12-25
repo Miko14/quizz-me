be39f5ae7605e661083aecf87bd6698d9fd4a53b
mslezak <michal_slezak@tempest.sk>
2017-12-26 00:28:15 +0100

    ready for FILE INPUT

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'questions',
@@ -11,6 +12,9 @@ export class QuestionsComponent implements OnInit {
   index: number = 0;
   score: number = 0;
   notFinished: boolean = true;
+  textInput: string;
+
+  questionType: string;
 
   questions: string[] = [
     "Akej farby je obloha?",
@@ -26,7 +30,7 @@ export class QuestionsComponent implements OnInit {
     "25", "30", "29", "31"
   ];
 
-  correctAnswer: string[] = [
+  correctAnswers: string[] = [
     "modrá", "pondelok", "31"
   ];
 
@@ -41,6 +45,7 @@ export class QuestionsComponent implements OnInit {
     this.number = 1;
     this.index = 0;
     this.score = 0;
+  
     this.notFinished = true;
     this.answers.splice(0, this.answers.length);
   }
@@ -56,11 +61,78 @@ export class QuestionsComponent implements OnInit {
     this.submitAnswer();
   }
 
+  readFile(inputFileValue: any): void {
+    var file: File = inputFileValue.files[0]; 
+    var myReader: FileReader = new FileReader();
+
+    // myReader.onloadend = function(e){
+    //   console.log(myReader.result);
+    // }
+
+    myReader.onloadend = () => {
+      this.textInput = myReader.result;
+      // console.log('===========================================================================')
+      // console.log(this.textInput);
+      // console.log('JSON PARSE: ');
+      // console.log(JSON.parse(this.textInput));
+      console.log('QQQQQQQQQQQQQQQQQQQQ:');
+      console.log(this.isJsonString(this.textInput));
+
+      /**
+       * checks if text from file is a valid JSON string
+       * then parse it according to specific structure
+       */
+      if (this.isJsonString(this.textInput)) {
+        let result = JSON.parse(this.textInput);
+        this.questions.splice(0, this.questions.length);
+        this.answersBank.splice(0, this.answersBank.length);
+        this.correctAnswers.splice(0, this.correctAnswers.length);
+        this.answers.splice(0, this.answers.length);
+        console.log(this.questions);
+        console.log(this.answersBank);
+        console.log(this.correctAnswers);
+        console.log(this.answers);
+        _(result.questions).forEach(item => {
+          this.questions.push(item.question);
+          this.questionType = item.questionType;
+          _(item.answers).forEach(element => {
+            this.answersBank.push(element);
+          });
+          _(item.correctAnswers).forEach(element => {
+            this.correctAnswers.push(element);
+          });
+        });
+
+        console.log('INITIALIZE ANSWERS only ONCE - must be fixed');
+        this.initializeAnswers();
+        console.log("aaa");
+        console.log(result);
+        console.log('=====================================================')
+        console.log(this.questions);
+        console.log(this.answersBank);
+        console.log(this.correctAnswers);
+      }
+
+    }
+    
+    myReader.readAsText(file);
+    
+  }
+
+  isJsonString(str): boolean {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
   submitAnswer(): void {
-    if (this.selectedAnswer == this.correctAnswer[this.number - 1]) {
+    if (this.selectedAnswer == this.correctAnswers[this.number - 1]) {
       this.score++;
     }
-    if (this.number == 3) {
+    if (this.number == this.questions.length) {
       this.notFinished = false;
     } else {
       this.number++;
